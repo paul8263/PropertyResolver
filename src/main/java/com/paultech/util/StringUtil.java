896c3d4573423bf3ed5f5bb2d20acaf214f8b2a4
Yao Zhang <xzhangyao@126.com>
2018-07-04 18:05:33 +0800

    Optimized string util

--- src/main/java/com/paultech/util/StringUtil.java
+++ src/main/java/com/paultech/util/StringUtil.java
@@ -1,28 +1,23 @@
 package com.paultech.util;
 
-import java.util.Arrays;
-import java.util.Optional;
-
 public class StringUtil {
     private StringUtil() {}
     public static String convertStringToCamelCase(String original, String delimiterRegex) {
-
-        Optional<String> reducedString = Arrays
-                .stream(original.split(delimiterRegex))
-                .map(StringUtil::capitalizeFirstLetter)
-                .reduce((s, s2) -> s + s2);
-        if (reducedString.isPresent()) {
-            return decapitalizeFirstLetter(reducedString.get());
-        } else {
-            return null;
+        String[] stringArray = original.split(delimiterRegex);
+        boolean firstSkipped = false;
+        StringBuilder sb = new StringBuilder(20);
+        for (String s : stringArray) {
+            if (!firstSkipped) {
+                sb.append(s);
+                firstSkipped = true;
+            } else {
+                sb.append(capitalizeFirstLetter(s));
+            }
         }
+        return sb.toString();
     }
 
     private static String capitalizeFirstLetter(String word) {
         return word.substring(0, 1).toUpperCase() + word.substring(1);
-    }
-
-    private static String decapitalizeFirstLetter(String word) {
-        return word.substring(0, 1).toLowerCase() + word.substring(1);
     }
 }
