f38c87ab6ea38440a4c8a5d6a4e34fe4d09f8ca7
Yao Zhang <xzhangyao@126.com>
2018-07-12 16:20:19 +0800

    Added after injection annotation support. Refactored codes.

--- src/main/java/com/paultech/resolver/PropertyResolver.java
+++ src/main/java/com/paultech/resolver/PropertyResolver.java
@@ -1,5 +1,6 @@
 package com.paultech.resolver;
 
+import com.paultech.annotation.AfterInjection;
 import com.paultech.annotation.FieldNamingPolicy;
 import com.paultech.annotation.PropertyName;
 import com.paultech.annotation.PropertySource;
@@ -10,6 +11,9 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileReader;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.Properties;
 import java.util.Set;
 
@@ -17,46 +21,79 @@ public class PropertyResolver {
     private static final Logger LOGGER = LoggerFactory.getLogger(PropertyResolver.class);
     public <T> T getProperty(Class<T> propertySourceClass) throws Exception {
         if (propertySourceClass.isAnnotationPresent(PropertySource.class)) {
-            PropertySource propertySource = propertySourceClass.getDeclaredAnnotation(PropertySource.class);
-            String propertyPath = new File(propertySource.value()).getAbsolutePath();
-            Properties properties = readPropertyFromPath(propertyPath);
-
-            FieldNamingPolicy fieldNamingPolicy = propertySource.fieldNamingPolicy();
-
             T propertySourceBean = propertySourceClass.newInstance();
 
-            Field[] declaredFields = propertySourceClass.getDeclaredFields();
-            for (Field declaredField : declaredFields) {
+            Properties properties = readProperties(propertySourceClass);
 
-                String fieldName = declaredField.getName();
+            injectPropertyValue(propertySourceClass, propertySourceBean, properties);
+            invokeAfterInjection(propertySourceClass, propertySourceBean, properties);
 
-                boolean isAssignedFieldName = false;
-                if (declaredField.isAnnotationPresent(PropertyName.class)) {
-                    PropertyName propertyNameAnnotation = declaredField.getDeclaredAnnotation(PropertyName.class);
-                    fieldName = propertyNameAnnotation.value();
-                    isAssignedFieldName = true;
-                }
-
-                Set<String> propertyNames = properties.stringPropertyNames();
-                for (String propertyName : propertyNames) {
-                    String convertedPropertyName = propertyName;
-
-                    if (!isAssignedFieldName) {
-                        convertedPropertyName = convertPropertyName(propertyName, fieldNamingPolicy);
-                    }
-
-                    if (fieldName.equals(convertedPropertyName)) {
-                        if (!declaredField.isAccessible()) {
-                            declaredField.setAccessible(true);
-                        }
-                        declaredField.set(propertySourceBean, properties.getProperty(propertyName));
-                        break;
-                    }
-                }
-            }
             return propertySourceBean;
         }
         return null;
+    }
+
+    private <T> Properties readProperties(Class<T> propertySourceClass) {
+        PropertySource propertySource = propertySourceClass.getDeclaredAnnotation(PropertySource.class);
+        String propertyPath = new File(propertySource.value()).getAbsolutePath();
+        return readPropertyFromPath(propertyPath);
+    }
+
+    private <T> void injectPropertyValue(Class<T> propertySourceClass, T propertySourceBean, Properties properties) throws IllegalAccessException {
+        PropertySource propertySource = propertySourceClass.getDeclaredAnnotation(PropertySource.class);
+        FieldNamingPolicy fieldNamingPolicy = propertySource.fieldNamingPolicy();
+
+        Field[] declaredFields = propertySourceClass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+
+            String fieldName = declaredField.getName();
+
+            boolean isAssignedFieldName = false;
+            if (declaredField.isAnnotationPresent(PropertyName.class)) {
+                PropertyName propertyNameAnnotation = declaredField.getDeclaredAnnotation(PropertyName.class);
+                fieldName = propertyNameAnnotation.value();
+                isAssignedFieldName = true;
+            }
+
+            Set<String> propertyNames = properties.stringPropertyNames();
+            for (String propertyName : propertyNames) {
+                String convertedPropertyName = propertyName;
+
+                if (!isAssignedFieldName) {
+                    convertedPropertyName = convertPropertyName(propertyName, fieldNamingPolicy);
+                }
+
+                if (fieldName.equals(convertedPropertyName)) {
+                    if (!declaredField.isAccessible()) {
+                        declaredField.setAccessible(true);
+                    }
+                    declaredField.set(propertySourceBean, properties.getProperty(propertyName));
+                    break;
+                }
+            }
+        }
+    }
+
+    private <T> void invokeAfterInjection(Class<T> propertySourceClass, T propertySourceBean, Properties properties) throws InvocationTargetException, IllegalAccessException {
+        Method[] declaredMethods = propertySourceClass.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            if (method.isAnnotationPresent(AfterInjection.class)) {
+
+                if (!method.isAccessible()) {
+                    method.setAccessible(true);
+                }
+
+                if (method.getParameterCount() == 0) {
+                    method.invoke(propertySourceBean);
+                } else if (method.getParameterCount() == 1) {
+                    Parameter parameter = method.getParameters()[0];
+                    if (parameter.getType().isAssignableFrom(Properties.class)) {
+                        method.invoke(propertySourceBean, properties);
+                    }
+                }
+
+            }
+        }
     }
 
     private String convertPropertyName(String propertyName, FieldNamingPolicy fieldNamingPolicy) {
